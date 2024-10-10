01f235790aff7dfff756b212936c37e25e924e38
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-10-10 09:52:01 +0530

    login stylings updated

--- my-react-app/src/components/FormContent/LoginFormContent.jsx
+++ my-react-app/src/components/FormContent/LoginFormContent.jsx
@@ -1,26 +1,33 @@
 import { Button, TextField } from "@mui/material";
 import React from "react";
+import loginimg from "../../assets/Login.jpg";
 
 export default function LoginFormContent(props) {
   return (
-    <div className="flex flex-col justify-center align-middle items-center h-screen w-auto   gap-5 border-2 border-cyan-700 mx-64 ">
-      <h1 className="text-6xl mb-8">{props.heading}</h1>
-      <TextField
-        helperText=" "
-        id="demo-helper-text-aligned-no-helper"
-        label={props.label1}
-      />
-      <TextField
-        helperText=" "
-        id="demo-helper-text-aligned-no-helper"
-        label={props.label2}
-      />
-      <Button variant="contained" size="medium">
-        {props.submitButton}
-      </Button>
-      <div className="flex gap-2 flex-row">
-        <p>{props.text}</p>
-        <Button size="small">{props.button}</Button>
+    <div className="grid w-100% h-auto grid-cols-2 mx-16 rounded-lg my-16 border-2 border-cyan-700 ">
+      <div>
+        <img src={loginimg} alt="login" />
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-5 align-middle rounded-md bg-gradient-to-t from-slate-200 to-cyan-900 ">
+        <h1 className="mb-12 text-6xl">{props.heading}</h1>
+        <TextField
+          helperText=" "
+          id="demo-helper-text-aligned-no-helper"
+          label={props.label1}
+        />
+        <TextField
+          helperText=" "
+          id="demo-helper-text-aligned-no-helper"
+          label={props.label2}
+        />
+        <Button variant="contained" size="medium">
+          {props.submitButton}
+        </Button>
+        <div className="flex flex-row gap-2">
+          <p>{props.text}</p>
+          <Button size="small">{props.button}</Button>
+        </div>
       </div>
     </div>
   );
